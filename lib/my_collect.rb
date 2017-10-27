bb083eec7a4350c9d8787804069967c4a3bad9c9
YanChan  Lin <yonalin1@gmail.com>
2017-10-27 16:22:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,23 @@
+def my_collect (collection)
+  if block_given?
+    i = 0
+    new_collection = []
+    while i < collection.length
+    new_collection << yield(collection[i])
+      i += 1
+    end
+    new_collection
+  else
+    puts "Hey! No block was given!"
+  end
+end
 
 
+# collection = ["ruby", "javascript", "python", "objective-c"]
+# my_collect(collection) do |lang|
+#
+# end
+#  collection = []
+#     my_collect(collection) do |student|
+#       student.first
+#     end
