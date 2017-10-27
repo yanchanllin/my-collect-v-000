24a188aeef1ce94830714a0b49d2b8bcf1698c7e
YanChan  Lin <yonalin1@gmail.com>
2017-10-27 16:21:28 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,26 +1,23 @@
 def my_collect (collection)
   if block_given?
-     i = 0
-   while i < collection.length
-      yield(collection[i])
-     i += 1
- # elsif collection = ["ruby", "javascript", "python", "objective-c"]
- #    i.upcase
- #  elsif collection = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
- #    i.first
-   end
-   collection
- else
-  puts "Hey! No block was given!"
- end
- end
+    i = 0
+    new_collection = []
+    while i < collection.length
+    new_collection << yield(collection[i])
+      i += 1
+    end
+    new_collection
+  else
+    puts "Hey! No block was given!"
+  end
+end
 
 
- # collection = ["ruby", "javascript", "python", "objective-c"]
- # my_collect(collection) do |lang|
- #
- # end
- #  collection = []
- #     my_collect(collection) do |student|
- #       student.first
- #     end
+# collection = ["ruby", "javascript", "python", "objective-c"]
+# my_collect(collection) do |lang|
+#
+# end
+#  collection = []
+#     my_collect(collection) do |student|
+#       student.first
+#     end
