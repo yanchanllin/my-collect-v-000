431f844a8bbebc7a6d3d0b3fd2174b0538c99357
YanChan  Lin <yonalin1@gmail.com>
2017-10-27 15:26:51 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,26 @@
+def my_collect (collection)
+  if block_given?
+     i = 0
+   while i < collection.length
+      yield(collection[i])
+     i += 1
+ # elsif collection = ["ruby", "javascript", "python", "objective-c"]
+ #    i.upcase
+ #  elsif collection = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
+ #    i.first
+   end
+   collection
+ else
+  puts "Hey! No block was given!"
+ end
+ end
 
 
+ # collection = ["ruby", "javascript", "python", "objective-c"]
+ # my_collect(collection) do |lang|
+ #
+ # end
+ #  collection = []
+ #     my_collect(collection) do |student|
+ #       student.first
+ #     end
